3b4be9d12a93fa1f9718b4b2a240d67488f1a68e
Arjun Kalburgi <askalburgi@gmail.com>
2021-02-17 16:10:58 -0500

    init contact list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,51 +6,70 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <section>
+      <h3>Static contact</h3>
+      <nylas-contact-list
+        id="428e0de5-9a87-4257-b20c-714d77dfd8c7"
+        :contacts="contacts" />
+    </section>
+    <section>
+      <h3>No contacts</h3>
+      <nylas-contact-list
+        id="428e0de5-9a87-4257-b20c-714d77dfd8c7"
+        :contacts="noContacts" />
+    </section>
   </div>
 </template>
 
 <script>
+import '@nylas/components-contact-list';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  }, 
+  data () {
+    return {
+      noContacts: [],
+      contacts: [
+      {
+        account_id: "none",
+        birthday: null,
+        company_name: null,
+        emails: [{ email: "nylascypresstest@gmail.com", type: "work" }],
+        given_name: "Nylas Contact #1",
+        id: "string",
+        source: null,
+        im_addresses: [],
+        job_title: null,
+        manager_name: null,
+        middle_name: null,
+        nickname: null,
+        notes: null,
+        object: "object",
+        office_location: null,
+        phone_numbers: [],
+        physical_addresses: [],
+        picture_url: null,
+        suffix: null,
+        surname: null,
+        web_pages: [],
+        groups: [],
+      }
+    ]
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+section {
+  padding: 40px;
+  background-color: #42b983;
+  border-bottom: 4px solid white;
+}
 h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 20px 0 20px;
 }
 a {
   color: #42b983;
